05df91894e057895c8578b9dbe7ddf310f2eafd2
stendall999 <natanstendall@mail.ru>
2024-12-12 18:09:14 +0800

    Initial commit of local

---
+++ local/components/my_slider/slider.one/.description.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    "NAME" => "Слайдер изображений",
+    "DESCRIPTION" => "Компонент для отображения слайдера изображений",
+    "PATH" => [
+        "ID" => "my_slider",
+        "NAME" => "Мои компоненты",
+    ],
+];
+?>
