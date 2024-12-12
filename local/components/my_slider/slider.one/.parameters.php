b48dbd66918c63b55660abc3230e47e8bde82e0b
stendall999 <natanstendall@mail.ru>
2024-12-12 22:57:50 +0800

    Восстановлена папка local

---
+++ local/components/my_slider/slider.one/.parameters.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arTemplateParameters = [
+    "IMAGES" => [
+        "NAME" => "Изображения",
+        "TYPE" => "TEXTAREA",
+        "ROWS" => 5,
+        "COLS" => 50,
+        "DEFAULT" => "",
+        "REFRESH" => "Y",
+    ],
+];
+?>
